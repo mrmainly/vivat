4994e451ac725b8b6b371db4314b8aad3ad8c1c6
ernest <ernest_everstov@mail.ru>
2022-06-23 14:02:46 +0900

    added text in i18n messages

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -1,19 +1,24 @@
-import { LOCALES } from './locales'
+import { LOCALES } from "./locales";
 
 export const messages = {
     [LOCALES.RUSSIAN]: {
-        langue_select: 'Выбор языка',
-        how_place_order: 'Как сделать заказ',
-        pharmancy_address: 'Aдрес аптеки',
-        search_textfield: 'Поиск по названию',
-        city: 'Город'
-
+        langue_select: "Выбор языка",
+        how_place_order: "Как сделать заказ",
+        pharmancy_address: "Aдрес аптеки",
+        search_textfield: "Поиск по названию",
+        city: "Город",
+        auth_true_toast: "авторизация прошла успешно",
+        pharmacy: "наши аптеки",
+        status_order: "статус заказа",
     },
     [LOCALES.SAKHA]: {
-        langue_select: 'Tылы талыы',
-        how_place_order: 'Cакааһы хайдах оҥорор',
-        pharmancy_address: 'Aптека аадырыһа',
-        search_textfield: 'табаар аатынан',
-        city: 'Kуорат'
-    }
-}
+        langue_select: "Tылы талыы",
+        how_place_order: "Cакааһы хайдах оҥорор",
+        pharmancy_address: "Aптека аадырыһа",
+        search_textfield: "табаар аатынан",
+        city: "Kуорат",
+        auth_true_toast: "авторизация ситиһиилээхтик ааста",
+        pharmacy: "аптекаларбыт",
+        status_order: "сакаас статуһа",
+    },
+};
