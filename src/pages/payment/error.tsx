94f452b9db50339a89e75e682a877db86b22ec32
ernest <ernest_everstov@mail.ru>
2022-06-30 07:10:20 +0900

    added success and error page

--- src/pages/payment/error.tsx
+++ src/pages/payment/error.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import { Box, Button } from "@mui/material";
+import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+import ErrorIcon from "@mui/icons-material/Error";
+
+import { MyText } from "../../components";
+import ROUTES from "../../routes";
+import ThemeMain from "../../theme";
+
+const Root = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+}));
 
 const ErrorPayment = () => {
-    return (
-        <div>
-            error
-        </div>
-    )
-}
+    const navigate = useNavigate();
 
-export default ErrorPayment
+    return (
+        <Root>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+                <MyText variant="h6">Оплата не прошла</MyText>
+                <ErrorIcon sx={{ color: "red", ml: 1 }} />
+            </Box>
+            <Button
+                sx={{ mt: 3, bgcolor: ThemeMain.palette.primary.main }}
+                variant="contained"
+                onClick={() => navigate(ROUTES.HOME)}
+            >
+                Вернуться на главную страницу
+            </Button>
+            {/* <MyText variant="body1"></MyText> */}
+        </Root>
+    );
+};
+
+export default ErrorPayment;
--- src/pages/payment/success.tsx
+++ src/pages/payment/success.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+
+import { Box, Button } from "@mui/material";
+import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+import AddTaskIcon from "@mui/icons-material/AddTask";
+
+import { MyText } from "../../components";
+import ROUTES from "../../routes";
+import ThemeMain from "../../theme";
+
+const Root = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+}));
 
 const SuccessPayment = () => {
-    return (
-        <div>
-            Success
-        </div>
-    )
-}
+    const navigate = useNavigate();
 
-export default SuccessPayment
+    return (
+        <Root>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+                <MyText variant="h6">Оплата прошла успешно</MyText>
+                <AddTaskIcon
+                    sx={{ color: ThemeMain.palette.primary.main, ml: 1 }}
+                />
+            </Box>
+            <Button
+                sx={{ mt: 3, bgcolor: ThemeMain.palette.primary.main }}
+                variant="contained"
+                onClick={() => navigate(ROUTES.STATUS_PRODUCT)}
+            >
+                Посмотреть статус заказа
+            </Button>
+            {/* <MyText variant="body1"></MyText> */}
+        </Root>
+    );
+};
+
+export default SuccessPayment;
